3618916700db3548bffc73461b1bcb82980ffdf4
Kanishk <kan@Kans-MacBook-Air.local>
2018-10-17 09:21:00 +0530

    initiate the domain name change

--- camping/settings/production.py
+++ camping/settings/production.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = [".campingcoindia.com"]
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = [".campingcoindia.com"]
+# ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
