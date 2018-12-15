ae58ab7b4fa5f1175d05602c8c12e8735c5e875d
Kanishk <kan@Kans-MacBook-Air.local>
2018-12-15 11:32:03 +0530

    debub = True

--- camping/settings/production.py
+++ camping/settings/production.py
@@ -26,7 +26,7 @@ STATIC_ROOT_DIR = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".camping-co.com"]
 # ALLOWED_HOSTS = [".herokuapp.com"]
