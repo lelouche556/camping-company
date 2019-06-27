ab3b3e46fe91d5028ced2d6ce66297a97af3c89e
Kanishk <kan@Kans-MacBook-Air.local>
2019-06-27 17:07:47 +0530

    Debug turn false

--- vehicle/admin.py
+++ vehicle/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from vehicle.models import VehicleCheck, VehicleDefinition
+from vehicle.models import VehicleCheck, VehicleDef
 
 # Register your models here.
 
 admin.site.register(VehicleCheck)
-admin.site.register(VehicleDefinition)
+admin.site.register(VehicleDef)
--- vehicle/models.py
+++ vehicle/models.py
@@ -28,7 +28,7 @@ class VehicleCheck(models.Model):
         return self.user.username
 
 
-class VehicleDefinition(models.Model):
+class VehicleDef(models.Model):
     car_name = models.CharField(max_length=64, blank=True, null=True)
     seat = models.CharField(max_length=12, blank=True, null=True)
     year = models.CharField(max_length=4, blank=True, null=True)
@@ -38,7 +38,6 @@ class VehicleDefinition(models.Model):
     color = models.CharField(max_length=12, blank=True, null=True)
     car_image = models.ImageField(upload_to="cars", blank=True, null=True)
     duration = models.IntegerField()
-    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.car_name
