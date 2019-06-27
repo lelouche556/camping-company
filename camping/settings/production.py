94e5117bc5c2e221501a84f73a9620ef30636b90
Kanishk <kan@Kans-MacBook-Air.local>
2019-06-27 15:19:21 +0530

    problem solve table does not exist

--- camping/settings/production.py
+++ camping/settings/production.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'destination',
     'blog',
     'referral',
-    'payment',
+    #'payment',
 ]
 
 MIDDLEWARE = [
--- payment/models.py
+++ payment/models.py
@@ -1,15 +1,15 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Payment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payment")
-    txnid = models.CharField(max_length=128, blank=True, null=True)
-    amount = models.IntegerField(blank=True, null=True)
-    firstname = models.CharField(max_length=32, blank=True, null=True)
-    email = models.CharField(max_length=128, blank=True, null=True)
-    productinfo = models.CharField(max_length=32, default='car book')
-    phone = models.BigIntegerField(blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# class Payment(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payment")
+#     txnid = models.CharField(max_length=128, blank=True, null=True)
+#     amount = models.IntegerField(blank=True, null=True)
+#     firstname = models.CharField(max_length=32, blank=True, null=True)
+#     email = models.CharField(max_length=128, blank=True, null=True)
+#     productinfo = models.CharField(max_length=32, default='car book')
+#     phone = models.BigIntegerField(blank=True, null=True)
+#
+#     def __str__(self):
+#         return self.user.username
